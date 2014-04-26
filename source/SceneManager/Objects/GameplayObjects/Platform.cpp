f3268082f55155b24811ef44bac4476e534e3e7f
Luc Charbonneau <charbonneau.luc@outlook.com>
2014-04-26 11:49:53 -0400

    Add platform object and update filters

---
+++ source/SceneManager/Objects/GameplayObjects/Platform.cpp
@@ -0,0 +1,12 @@
+#include "SceneManager/Objects/GameplayObjects/GamePlayObject.h"
+#include "SceneManager/SceneManager.h"
+
+#include "Utilities/BasicGeometry/BasicGeometryGenerator.h"
+
+namespace Atum
+{
+namespace SceneManager
+{
+
+} // namespace SceneManager
+} // namespace Atum
---
+++ source/SceneManager/Objects/GameplayObjects/Platform.h
@@ -0,0 +1,18 @@
+#ifndef SCENEMANAGER_GAMEPLAYOBJECT_H
+#define SCENEMANAGER_GAMEPLAYOBJECT_H
+
+#include "SceneManager/Objects/Object.h"
+#include "SceneManager/Manager/TypedefID.h"
+
+namespace Atum { namespace SceneManager { class PropertyList; }}
+
+namespace Atum
+{
+namespace SceneManager
+{
+
+} // namespace SceneManager
+} // namespace Atum
+
+
+#endif
